81928bcef043bc01afbca8a6d088774552cbc743
Vanesa Villafana Liñan <denisevillafana@hotmail.com>
2025-07-01 15:22:42 -0300

    feat: busqueda por nombre y numero de figurita
    Relacionado con:
    M2 - HU01
    M2 - HU02

--- Codigo Fuente/tufistore/resources/views/articulos/show.blade.php
+++ Codigo Fuente/tufistore/resources/views/articulos/show.blade.php
@@ -1,7 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+<!--BUSQUEDA DE FIGURITAS POR NOMBRE Y NUMERO-->
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header fw-bold">{{ $articulo->nombre }}</div>
+                    <div class="card-body" style="min-height: 500px">
+                        {{ $articulo->nombre }}
+                    </div>
+                    <div class="card-footer">
+                        <a href="{{ route('articulos.index') }}" class="btn btn-primary"> Volver a tienda </a> <!--ESTE TIENE QUE VOLVER A LA PAGINA TIENDA.PHP-->
+                    </div>
+                    <div class="card">
+                    <div class="card-header fw-bold">{{ $articulo->numero }}</div>
+                    <div class="card-body" style="min-height: 500px">
+                        {{ $articulo->numero }}
+                    </div>
+                    <div class="card-footer">
+                        <a href="{{ route('articulos.index') }}" class="btn btn-primary"> Volver a tienda </a> <!--ESTE TIENE QUE VOLVER A LA PAGINA TIENDA.PHP-->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+<div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
